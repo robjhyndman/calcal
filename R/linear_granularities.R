503135b855e5f9ad669fedd5bdc176458933c077
Rob J Hyndman <robjhyndman@gmail.com>
2025-05-23 13:48:57 +1000

    Generalized linear_granularity to linear_cyclic to handle any combinations of linear and cyclic granularities

--- R/linear_granularities.R
+++ R/linear_granularities.R
@@ -1,105 +1,137 @@
-#' @title Linear Granularity
+#' @title Create Linear-Cyclic Granularity Combinations
 #' @description
-#' Linear granularities are produced by combining the year and a cyclic granularity.
-#' @param gran_fun A function that extracts a cyclic granularity from a date
-#' variable. This function must return an integer.
-#' @param gran_levels A character vector of the granularity levels.
-#' @param gran_name A character string giving the name of the granularity
+#' Linear-cyclic granularity combinations are produced by combining two granularities, one nested within the other.
+#' For example, the year and the month, or the week and the day-of-week. It is assumed that the
+#' first granularity is linear (i.e., it increases over time), and the second granularity is cyclic and nested within the first granularity.
+#' @param gran_fun1 A function that extracts a linear granularity from a date.
+#' This function must return an integer.
+#' @param gran_fun2 A function that extracts a cyclic granularity from a date, nested within `gran_fun1`.
+#' This function must return an integer.
+#' @param gran_levels A character vector of the granularity levels for `gran_fun2`.
+#' @param gran_name A character string giving the name of the linear granularity.
 #' @return A function that creates a linear granularity object.
 #' @examples
-#' # Compute quarter from a Gregorian date
+#' # Year-quarter granularity
+#' ## Compute quarter from a Gregorian date
 #' quarter <- function(date) {
 #'   ceiling(month_of_year(date) / 3)
 #' }
-#' year_quarter <- linear_granularity(quarter, paste0("Q", 1:4), "year-quarter")
+#' year_quarter <- linear_cyclic(year, quarter, paste0("Q", 1:4), "year-quarter")
 #' year_quarter(gregorian_date(2025, 6, 25) + 1:10)
+#'
+#' # Week-day granularity
+#' ## Compute week as linear granularity
+#' week <- function(date) {
+#'   (seq_along(date) - 1) %/% 7 + 1
+#' }
+#' ## Compute day as numerical cyclic granularity
+#' day <- function(date) {
+#'   day_of_week(date, numeric = TRUE)
+#' }
+#' week_day <- linear_cyclic(
+#'   week,
+#'   day,
+#'   c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"),
+#'   gran_name = "week-day"
+#' )
+#' week_day(gregorian_date(2025, 6, 25) + 1:10)
 #' @export
-linear_granularity <- function(gran_fun, gran_levels, gran_name) {
+linear_cyclic <- function(gran_fun1, gran_fun2, gran_levels, gran_name) {
   function(date) {
-    lin_gran(year(date), gran_fun(date), gran_levels, gran_name)
+    lin_cyc_gran(gran_fun1(date), gran_fun2(date), gran_levels, gran_name)
   }
 }
 
-lin_gran <- function(
-    year = integer(),
-    gran = integer(),
-    gran_levels = character(),
-    gran_name = character()) {
+lin_cyc_gran <- function(
+  gran1 = integer(),
+  gran2 = integer(),
+  gran_levels = character(),
+  gran_name = character()
+) {
   lst <- vec_recycle_common(
-    year = year,
-    gran = factor(gran, levels = seq_along(gran_levels), labels = gran_levels, ordered = TRUE),
-    .size = max(length(year), length(gran))
+    gran1 = gran1,
+    gran2 = factor(
+      gran2,
+      levels = seq_along(gran_levels),
+      labels = gran_levels,
+      ordered = TRUE
+    ),
+    .size = max(length(gran1), length(gran2))
   )
-  output <- new_rcrd(lst, class = "linear_granularity")
+  if (any(diff(lst$gran1) < 0)) {
+    stop("gran1 must be strictly increasing")
+  }
+  if (any(gran2 > length(gran_levels))) {
+    stop("gran2 must be in 1:", length(gran_levels))
+  }
+  output <- new_rcrd(lst, class = "linear_cyclic")
   attr(output, "name") <- gran_name
   return(output)
 }
 
 #' @export
-format.linear_granularity <- function(x, ...) {
-  paste0(field(x, "year"), "-", field(x, "gran"))
+format.linear_cyclic <- function(x, ...) {
+  paste0(field(x, "gran1"), "-", field(x, "gran2"))
 }
 
 #' @export
-as.character.linear_granularity <- function(x, ...) {
+as.character.linear_cyclic <- function(x, ...) {
   format(x, ...)
 }
 
 #' @export
-vec_ptype_full.linear_granularity <- function(x, ...) {
+vec_ptype_full.linear_cyclic <- function(x, ...) {
   attributes(x)$name
 }
 
 #' @export
-vec_ptype2.linear_granularity.linear_granularity <- function(x, y, ...) {
-  lin_gran()
+vec_ptype2.linear_cyclic.linear_cyclic <- function(x, y, ...) {
+  lin_cyc_gran()
 }
 
 #' @export
-vec_cast.linear_granularity.linear_granularity <- function(x, to, ...) x
+vec_cast.linear_cyclic.linear_cyclic <- function(x, to, ...) x
 #' @export
-vec_cast.double.linear_granularity <- function(x, to, ...) {
-  field(x, "year") +
-    as.numeric(field(x, "gran")) / length(levels(field(x, "gran")))
+vec_cast.double.linear_cyclic <- function(x, to, ...) {
+  field(x, "gran1") +
+    as.numeric(field(x, "gran2")) / length(levels(field(x, "gran2")))
 }
 
 # Arithmetic
 
 #' @export
-#' @method vec_arith linear_granularity
-vec_arith.linear_granularity <- function(op, x, y, ...) {
-  UseMethod("vec_arith.linear_granularity", y)
+#' @method vec_arith linear_cyclic
+vec_arith.linear_cyclic <- function(op, x, y, ...) {
+  UseMethod("vec_arith.linear_cyclic", y)
 }
 
 #' @export
-#' @method vec_arith.linear_granularity linear_granularity
-vec_arith.linear_granularity.linear_granularity <- function(op, x, y, ...) {
-  switch(op,
-    "-" = vec_arith_base(op, x, y),
-    stop_incompatible_op(op, x, y)
-  )
+#' @method vec_arith.linear_cyclic linear_cyclic
+vec_arith.linear_cyclic.linear_cyclic <- function(op, x, y, ...) {
+  switch(op, "-" = vec_arith_base(op, x, y), stop_incompatible_op(op, x, y))
 }
 
 #' @export
-#' @method vec_arith.numeric linear_granularity
-vec_arith.numeric.linear_granularity <- function(op, x, y, ...) {
+#' @method vec_arith.numeric linear_cyclic
+vec_arith.numeric.linear_cyclic <- function(op, x, y, ...) {
   vec_arith(op, y, x)
 }
 
 #' @export
-#' @method vec_arith.linear_granularity numeric
-vec_arith.linear_granularity.numeric <- function(op, x, y, ...) {
-  nlevels <- length(levels(field(x, "gran")))
-  new_gran <- switch(op,
-    "+" = vec_arith_base(op, field(x, "gran"), y),
-    "-" = vec_arith_base(op, field(x, "gran"), y),
+#' @method vec_arith.linear_cyclic numeric
+vec_arith.linear_cyclic.numeric <- function(op, x, y, ...) {
+  nlevels <- length(levels(field(x, "gran2")))
+  new_gran <- switch(
+    op,
+    "+" = vec_arith_base(op, field(x, "gran2"), y),
+    "-" = vec_arith_base(op, field(x, "gran2"), y),
     stop_incompatible_op(op, x, y)
   )
-  field(x, "year") <- field(x, "year") + (new_gran - 1) %/% nlevels
-  field(x, "gran") <- factor(
+  field(x, "gran1") <- field(x, "gran1") + (new_gran - 1) %/% nlevels
+  field(x, "gran2") <- factor(
     (new_gran - 1) %% nlevels + 1,
-    levels = seq_along(levels(field(x, "gran"))),
-    labels = levels(field(x, "gran")),
+    levels = seq_along(levels(field(x, "gran2"))),
+    labels = levels(field(x, "gran2")),
     ordered = TRUE
   )
   return(x)
--- tests/testthat/test-granularity.R
+++ tests/testthat/test-granularity.R
@@ -1,15 +1,11 @@
 test_that("granulaities", {
   yr2025 <- gregorian_date(2025, 1, 1) + 0:364
-  year_month <- linear_granularity(month_of_year, month.abb, "year-month")
+  year_month <- linear_cyclic(year, month_of_year, month.abb, "year-month")
   expect_equal(
     as.character(year_month(yr2025)),
     gsub(" ", "-", as.character(tsibble::yearmonth(as.Date(yr2025))))
   )
   expect_equal(year_month(yr2025) + 1, 1 + year_month(yr2025))
-  expect_equal(
-    year(year_month(yr2025) + 1),
-    c(rep(2025, 365 - 31), rep(2026, 31))
-  )
   expect_equal(
     year_month(yr2025) |> as.numeric(),
     c(2025 + month_of_year(yr2025) / 12)
