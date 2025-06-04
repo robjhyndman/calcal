b32a68c5bb85bd391783b881d5edd08838c2f9bd
Rob J Hyndman <robjhyndman@gmail.com>
2025-06-04 10:56:13 +1000

    Improved docs for granularities

--- R/granularities.R
+++ R/granularities.R
@@ -1,13 +1,14 @@
 #' Find and extract canonical granularities from a calendar object
 #'
-#' `granularities()` will return a character vector of granularity names.
+#' `granularities()` will return a character vector of granularity names for the
+#' relevant calendar.
 #' `granularity()` will return a vector of numerical values for a given granularity.
 #'
 #' @param dates A vector of calendar dates
 #' @rdname granularities
 #' @examples
-#' granularities(iso_date(2025, 23, 2:4))
-#' granularities(gregorian_date(2025, 4, 19:30))
+#' granularities(iso_date(2025, 23, 2))
+#' granularities(gregorian_date(2025, 4, 19))
 #' granularities(as_roman(Sys.Date()))
 #' @export
 granularities <- function(dates) {
@@ -17,9 +18,9 @@ granularities <- function(dates) {
 #' @param granularity A character string indicating the granularity to extract
 #' @rdname granularities
 #' @examples
-#' granularity(iso_date(2025, 23, 2:4), "week")
-#' granularity(gregorian_date(2025, 4, 19:21), "month")
-#' granularity(as_roman(Sys.Date()), "event")
+#' granularity(iso_date(2025, 23, 2), "week")
+#' granularity(gregorian_date(2025, 4, 19), "month")
+#' granularity(as_roman(Sys.Date()), "count")
 #' @export
 granularity <- function(dates, granularity) {
   vctrs::field(dates, granularity)
