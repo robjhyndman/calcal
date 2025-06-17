d0a9bf03326ab4313af9114ab6d5aecb7ff0ec8f
Rob J Hyndman <robjhyndman@gmail.com>
2025-06-17 13:47:59 +1000

    Fixed hindu lunar test

--- tests/testthat/test-hindu.R
+++ tests/testthat/test-hindu.R
@@ -26,10 +26,10 @@ test_that("old_hindu_lunar", {
     gregorian_date(1967, 5, 2) |> as.numeric()
   )
   expect_equal(
-    old_hindu_lunar_date(5126, 3, 1:10) |>
+    old_hindu_lunar_date(5126, 3, FALSE, 1:10) |>
       cal_old_hindu_lunar$from_rd() |>
       cal_old_hindu_lunar$to_rd(),
-    old_hindu_lunar_date(5126, 3, 1:10) |> vctrs::vec_data()
+    old_hindu_lunar_date(5126, 3, FALSE, 1:10) |> vctrs::vec_data()
   )
   expect_no_error(
     as_old_hindu_lunar(1:1e5) |> as.list() |> validate_old_hindu_lunar()
